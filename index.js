9f855e0fb36a02abc4c3d144e13fc29df52e289e
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-01-04 11:07:00 +0100

    Connect server to MongoDB database

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 // NPM Packages
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
+
+mongoose.set('strictQuery', false);
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log('Server connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Error connecting server to MongoDB:', error.message);
+  });
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
