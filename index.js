45d94e44a3e360c1aed0a9fbd1d154f6f07a58a9
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-01-04 11:47:48 +0100

    Add blog model & create blog route

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const app = express();
 
 mongoose.set('strictQuery', false);
 
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
 const mongoUrl = process.env.MONGODB_URI;
 mongoose
   .connect(mongoUrl)
@@ -17,6 +26,24 @@ mongoose
   .catch((error) => {
     console.log('Error connecting server to MongoDB:', error.message);
   });
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/blogs', (request, response) => {
+  const { title, author, url, likes } = request.body;
+
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes,
+  });
+
+  blog.save().then((savedBlog) => {
+    response.status(201).json(savedBlog);
+  });
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
