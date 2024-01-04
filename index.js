1eec5f1d202c0e197cd564d6d48a8f856130e3de
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-01-04 10:52:31 +0100

    Set up server

---
+++ index.js
@@ -0,0 +1,10 @@
+// NPM Packages
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
