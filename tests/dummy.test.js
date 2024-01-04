d5966deec0afc392a88d63217ffd43a542edd29f
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-01-04 18:00:05 +0100

    Add jest test for dummy helper function

---
+++ tests/dummy.test.js
@@ -0,0 +1,9 @@
+// Local Files
+const listHelper = require('../utils/list_helper');
+
+test('dummy returns one', () => {
+  const blogs = [];
+
+  const result = listHelper.dummy(blogs);
+  expect(result).toBe(1);
+});
