51502de4958e809325c7212ff94c00c423396a40
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-01-04 14:41:23 +0100

    Create app module

---
+++ app.js
@@ -0,0 +1,28 @@
+// NPM Packages
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// Local Files
+const blogsRouter = require('./controllers/blogs');
+
+const app = express();
+
+mongoose.set('strictQuery', false);
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log('Server connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Error connecting server to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,30 +1,8 @@
 // NPM Packages
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Local Files
-const blogRouter = require('./controllers/blogs');
-
-const app = express();
-
-mongoose.set('strictQuery', false);
-
-const mongoUrl = process.env.MONGODB_URI;
-mongoose
-  .connect(mongoUrl)
-  .then(() => {
-    console.log('Server connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('Error connecting server to MongoDB:', error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs', blogRouter);
+const app = require('./app');
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
