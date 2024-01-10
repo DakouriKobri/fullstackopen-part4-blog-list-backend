daea6d31548b1caf6c2a304de8ac1ac1f4b53c73
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-01-10 22:35:34 +0100

    Refactor: grouping tests under describe

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -14,121 +14,129 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs);
 });
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test('unique identifier of each blog post is id', async () => {
+    const response = await api.get('/api/blogs');
+
+    for (const blog of response.body) {
+      expect(blog.id).toBeDefined();
+    }
+  });
 });
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs');
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
+describe('addition of a new blog', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Top 10 Node.js Debugging Tips to Debug Like a Pro',
+      author: 'Lou',
+      url: 'https://stackify.com/node-js-debugging-tips/',
+      likes: 41,
+    };
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd.length).toEqual(helper.initialBlogs.length + 1);
+
+    const titles = blogsAtEnd.map((blog) => blog.title);
+    expect(titles).toContain(newBlog.title);
+  });
+
+  test('missing likes defaults to 0', async () => {
+    const newBlog = {
+      title: 'Understanding memory leaks in Node.js apps',
+      // eslint-disable-next-line quotes
+      author: "Faith Ng'etich",
+      url: 'https://blog.logrocket.com/understanding-memory-leaks-node-js-apps/',
+    };
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    const savedBlog = blogsAtEnd.find((blog) => blog.title === newBlog.title);
+    expect(savedBlog.likes).toEqual(0);
+  });
+
+  test('missing title returns status code 400', async () => {
+    const newBlog = {
+      author: 'John Doe',
+      url: 'http://example.com/johndoes-blog',
+    };
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('missing url returns status code 400', async () => {
+    const newBlog = {
+      title: 'The Art of coding RESTful APIs',
+      author: 'John Doe',
+    };
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+  });
 });
 
-test('unique identifier of each blog post is id', async () => {
-  const response = await api.get('/api/blogs');
+describe('deletion of a blog', () => {
+  test('delete a blog returns statusCode 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
 
-  for (const blog of response.body) {
-    expect(blog.id).toBeDefined();
-  }
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd.length).toEqual(helper.initialBlogs.length - 1);
+
+    const titles = blogsAtEnd.map((blog) => blog.title);
+    expect(titles).not.toContain(blogToDelete.title);
+  });
 });
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Top 10 Node.js Debugging Tips to Debug Like a Pro',
-    author: 'Lou',
-    url: 'https://stackify.com/node-js-debugging-tips/',
-    likes: 41,
-  };
+describe('editing a blog', () => {
+  test('successful blog update returns status code 200 with valid data', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToUpdate = blogsAtStart[0];
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    const blogUpdate = {
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 10,
+    };
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd.length).toEqual(helper.initialBlogs.length + 1);
+    await api.put(`/api/blogs/${blogToUpdate.id}`).send(blogUpdate).expect(200);
 
-  const titles = blogsAtEnd.map((blog) => blog.title);
-  expect(titles).toContain(newBlog.title);
-});
-
-test('missing likes defaults to 0', async () => {
-  const newBlog = {
-    title: 'Understanding memory leaks in Node.js apps',
-    // eslint-disable-next-line quotes
-    author: "Faith Ng'etich",
-    url: 'https://blog.logrocket.com/understanding-memory-leaks-node-js-apps/',
-  };
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  const savedBlog = blogsAtEnd.find((blog) => blog.title === newBlog.title);
-  expect(savedBlog.likes).toEqual(0);
-});
-
-test('missing title returns status code 400', async () => {
-  const newBlog = {
-    author: 'John Doe',
-    url: 'http://example.com/johndoes-blog',
-  };
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-    .expect('Content-Type', /application\/json/);
-});
-
-test('missing url returns status code 400', async () => {
-  const newBlog = {
-    title: 'The Art of coding RESTful APIs',
-    author: 'John Doe',
-  };
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-    .expect('Content-Type', /application\/json/);
-});
-
-test('delete a blog returns statusCode 204 if id is valid', async () => {
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToDelete = blogsAtStart[0];
-
-  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd.length).toEqual(helper.initialBlogs.length - 1);
-
-  const titles = blogsAtEnd.map((blog) => blog.title);
-  expect(titles).not.toContain(blogToDelete.title);
-});
-
-test('successful blog update returns status code 200 with valid data', async () => {
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToUpdate = blogsAtStart[0];
-
-  const blogUpdate = {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 10,
-  };
-
-  await api.put(`/api/blogs/${blogToUpdate.id}`).send(blogUpdate).expect(200);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  const updatedBlog = blogsAtEnd[0];
-  expect(updatedBlog.likes).toEqual(10);
+    const blogsAtEnd = await helper.blogsInDb();
+    const updatedBlog = blogsAtEnd[0];
+    expect(updatedBlog.likes).toEqual(10);
+  });
 });
 
 afterAll(async () => {
