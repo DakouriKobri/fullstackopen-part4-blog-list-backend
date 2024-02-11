65a53e06b91205bc759d8296b4e96133f70f5207
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-02-11 14:53:40 +0100

    Handle expired token

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,5 +1,9 @@
+// NPM Packages
+const jwt = require('jsonwebtoken');
+
 // Local Files
 const logger = require('./logger');
+const User = require('../models/user');
 
 function errorHandler(error, request, response, next) {
   logger.error(error.message);
@@ -9,6 +13,8 @@ function errorHandler(error, request, response, next) {
   } else if (error.name === 'ValidationError') {
     response.status(400).send({ error: error.message });
   } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: error.message });
+  } else if (error.name === 'TokenExpiredError') {
     return response.status(401).json({ error: error.message });
   }
 
@@ -30,7 +36,22 @@ function tokenExtractor(request, response, next) {
   }
 }
 
+async function userExtractor(request, response, next) {
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'Invalid token' });
+    }
+    const loggedInUser = await User.findById(decodedToken.id);
+    request.user = loggedInUser;
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   errorHandler,
+  userExtractor,
   tokenExtractor,
 };
